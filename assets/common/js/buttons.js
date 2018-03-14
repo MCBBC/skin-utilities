97649248acb04a4fc93ae8f6c73fd0b436753dfa
MCBBC <DOR2169244@maricopa.edu>
2018-03-14 23:19:08 +0800

    Update buttons.js
    Modifying the method of getting tid

--- assets/common/js/buttons.js
+++ assets/common/js/buttons.js
@@ -1,3 +1,3 @@
-tid = /\d+$/.exec(window.location.pathname)[0];
+tid = texture.tid;
 $('div.box-footer:first').append('<a href="/user/skin-utilities/editor-1?tid=' + tid + '" class="btn btn-default" target="_blank">编辑器一</a> ');
 $('div.box-footer:first').append('<a href="/user/skin-utilities/editor-2?tid=' + tid + '" class="btn btn-default" target="_blank">编辑器二</a>');
